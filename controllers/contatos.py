1ceb70190b8831a88ffd486f60db08e1e76b8ea9
Bruna Clares Cardoso <clares.bruna@gmail.com>
2025-04-02 19:22:21 -0300

    feat(logs): inclui registro de logs na inserção de contatos

--- controllers/contatos.py
+++ controllers/contatos.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
-from database import connection
+from database.database import connection, get_cursor
 import psycopg
+from services.logs import registrar_log
 
 contatos_bp = Blueprint("contatos", __name__)
 
@@ -9,29 +10,42 @@ def inserir_contato():
     novo_contato = request.get_json()
 
     if not novo_contato:
+        registrar_log("Erro de Validação", "Nenhum dado recebido")
         return jsonify({"erro": "Nenhum dado recebido"}), 400
 
     if not novo_contato.get("nome"):
+        registrar_log("Erro de Validação", "Nome obrigatório ausente")
         return jsonify({"erro": "O nome é obrigatório"}), 400
-    
+  
     if not novo_contato.get("mensagem"):
+        registrar_log("Erro de Validação", "Mensagem obrigatória ausente")
         return jsonify({"erro": "A mensagem é obrigatória"}), 400
-    
+  
     if not novo_contato.get("telefone") and not novo_contato.get("email"):
+        registrar_log("Erro de Validação", "Nenhum meio de contato fornecido")
         return jsonify({"erro": "Ao menos um contato é obrigatório"}), 400
-    
+  
     try:
-        with connection as conn:
-            with conn.cursor() as curs:
-                curs.execute(
-                    '''INSERT INTO contatos (nome,
-                    telefone, email, mensagem) VALUES(%s, %s, %s, %s)''',
-                    (novo_contato.get("nome"), novo_contato.get("telefone"),
-                        novo_contato.get("email"), novo_contato.get("mensagem")))
+        with get_cursor() as cur:
+            cur.execute(
+                '''INSERT INTO contatos (nome,
+                telefone, email, mensagem)
+                VALUES(%s, %s, %s, %s) RETURNING id
+                ''',
+                (novo_contato.get("nome"), novo_contato.get("telefone"),
+                    novo_contato.get("email"), novo_contato.get("mensagem")))
             
-    except psycopg.DatabaseError:
-        return jsonify({"erro": "Sua mensagem não foi entregue, tente novamente, por favor!"})
-    
-    return jsonify({"sucesso": "Sua mensagem foi enviada com sucesso!"})
-
-    
+            contato_id = cur.fetchone()['id']
+            
+        connection.commit()
+                
+        registrar_log("Contato Criado", f"ID {contato_id} registrado com sucesso")
+        
+        return jsonify({"sucesso": "Sua mensagem foi enviado com sucesso!"}), 201
+            
+    except psycopg.DatabaseError as e:
+        connection.rollback()
+        
+        registrar_log("Erro ao Salvar Contato", str(e))
+        
+        return jsonify({"erro": "Sua mensagem não foi entregue, tente novamente, por favor!"}), 500
