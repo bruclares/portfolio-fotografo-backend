fff5bc52e7606336573337e97791721212359bb7
Bruna Clares Cardoso <clares.bruna@gmail.com>
2025-04-11 14:27:54 -0300

    chore(app): registra blueprint de autenticação na aplicação

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
-
-import cloudinary
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
 
 from controllers.contatos import contatos_bp
 from controllers.cloudinaryapi import cloudinary_bp
+from controllers.auth import auth_bp
 
+
+load_dotenv()
 
 app = Flask(__name__)
 
 CORS(app)
+
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+
+jwt = JWTManager(app)
 
 
 @app.route("/")
@@ -19,10 +27,12 @@ def home():
 
 app.register_blueprint(contatos_bp, url_prefix="/api/contatos")
 app.register_blueprint(cloudinary_bp, url_prefix="/api/cloudinary")
+app.register_blueprint(auth_bp, url_prefix="/api/auth")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+    app.run(debug=debug_mode)
 
 
 # python app.py
