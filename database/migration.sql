5384a03b9955881e582381bb76e441facec3cf7d
Bruna Clares Cardoso <clares.bruna@gmail.com>
2025-04-11 14:43:31 -0300

    feat(migration): adiciona tabelas fotografo e contatos_fotografo para controle administrativo

--- database/migration.sql
+++ database/migration.sql
@@ -19,3 +19,23 @@ create table logs (
     metodo varchar(10),
     status varchar(50)
 );
+
+-- Tabela de administrador (fotógrafo) apenas um registro
+CREATE TABLE fotografo (
+id INT PRIMARY KEY CHECK (id = 1), 
+nome VARCHAR(255),
+email VARCHAR(255) NOT NULL,
+senha_hash VARCHAR(255) NOT NULL, 
+token_recuperacao VARCHAR(255),
+token_expiracao TIMESTAMP, 
+criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table contatos_fotografo (
+id SERIAL PRIMARY KEY, 
+tipo varchar(255),
+contato varchar(255),
+criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
